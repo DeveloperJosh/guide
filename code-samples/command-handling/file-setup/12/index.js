8265c7ff54291614791f11dd2e76d04aac421679
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-06-13 21:49:32 -0400

    Update index.js

--- code-samples/command-handling/file-setup/12/index.js
+++ code-samples/command-handling/file-setup/12/index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+const date = new Date();
+const fs = require('fs');
+const ms = require('ms');
+const moment = require('moment');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -17,20 +21,49 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.guild) return;
+    if (!message.member)
+        message.member = await message.guild.fetchMember(message);
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
+    const com = args.shift().toLowerCase();
+    if (com.length == 0) return;
+    const command =
+        client.commands.get(com) ||
+        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(com));
+    if (command) {
+        if (command.timeout) {
+            if (Timeout.has(`${message.author.id}${command.name}`)) {
+                let um = new Discord.MessageEmbed();
+                um.setTitle('Hold Up ✋!');
+                um.setDescription(
+                    `You have to wait more ${ms(
+                        command.timeout
+                    )}, to use this command again`
+                );
+                um.addField(
+                    'Why?',
+                    'Because this system was installed, in order not to flood the chat with bot commands everywhere',
+                    true
+                );
+                um.setFooter(`This message gets deleted after 10s`);
+                um.setTimestamp(new Date());
+                um.setColor(0xf94343);
+                return message
+                    .reply(um)
+                    .then(message => message.delete({ timeout: 10000 }));
+            } else {
+                Timeout.add(`${message.author.id}${command.name}`);
+                setTimeout(() => {
+                    Timeout.delete(`${message.author.id}${command.name}`);
+                }, command.timeout);
+            }
+        }
+        command.run(client, message, args);
+    }
 });
 
 client.login(token);
